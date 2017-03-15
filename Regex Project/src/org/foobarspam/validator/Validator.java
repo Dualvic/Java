57326ab6f5ac8786c75dd094104ed75c4a98306f
Victor Garcia Regadera <victorgarcia.esp@gmail.com>
2017-03-15 16:30:44 +0100

    Update Validator.java

--- Regex Project/src/org/foobarspam/validator/Validator.java
+++ Regex Project/src/org/foobarspam/validator/Validator.java
@@ -5,12 +5,15 @@ import java.util.regex.Pattern;
 
 public class Validator {
 	
-	static private String dniRegex = "\\b\\d{8}[A-Z&&[^IÑOU]]\\b";
+	// Regular expresion declaration
+	static private String dniRegex = "\\b\\d{8}[A-Z&&[^IÃ‘OU]]\\b";
 	static private String nieRegex = "\\b\\[XYZ]d{8}\\b";
 	
+	// Variables holding compiled regex
 	static Pattern dniFormat = Pattern.compile(dniRegex);
 	static Pattern nieFormat = Pattern.compile(nieRegex);
 	
+	// Logic - Validators
 	public static boolean dniValidator(String dni){
 		 Matcher verificador = dniFormat.matcher(dni);
 		 return verificador.find();
