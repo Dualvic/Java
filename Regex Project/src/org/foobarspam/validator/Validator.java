27a4a72d280f6181a4a56861563dc82ea6c31337
Victor Garcia Regadera <victorgarcia.esp@gmail.com>
2017-03-15 16:29:01 +0100

    Add files via upload

---
+++ Regex Project/src/org/foobarspam/validator/Validator.java
@@ -0,0 +1,23 @@
+package org.foobarspam.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+	
+	static private String dniRegex = "\\b\\d{8}[A-Z&&[^IÑOU]]\\b";
+	static private String nieRegex = "\\b\\[XYZ]d{8}\\b";
+	
+	static Pattern dniFormat = Pattern.compile(dniRegex);
+	static Pattern nieFormat = Pattern.compile(nieRegex);
+	
+	public static boolean dniValidator(String dni){
+		 Matcher verificador = dniFormat.matcher(dni);
+		 return verificador.find();
+	}
+	
+	public static boolean nieValidator(String nie) {
+		 Matcher verificador = nieFormat.matcher(nie);
+		 return verificador.find();
+	}
+}
